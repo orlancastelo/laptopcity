1354203589698525e6911d59cc738b3b3d9f2113
orlancastelo <casteloorlan@gmail.com>
2020-08-12 13:17:23 +0800

    upload all my files

---
+++ index.js
@@ -0,0 +1,96 @@
+$(document).ready(function(){
+
+    //Banner Owl Carousel 2
+    $("#banner-area .owl-carousel").owlCarousel({
+        dots:true,
+        items:1
+
+    });
+
+    //Top Sale Owl Carousel 2
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop:true,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+    //Isotope Filter
+    var $grid = $(".grid").isotope({
+        itemSelector:'.grid-item',
+        layoutMode:'fitRows'
+    });
+
+    //Filter items on button click
+    $(".button-group").on("click","button", function(){
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({filter: filterValue});
+    })
+
+     //New Phones Owl Carousel 2
+     $("#new-phones .owl-carousel").owlCarousel({
+        loop:true,
+        nav:false,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+    //Blogs Owl Carousel 2
+    $("#blogs .owl-carousel").owlCarousel({
+        loop:true,
+        nav:false,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            }
+        }
+    })
+
+    //Product Quantity Section
+    let $qty_up = $(".qty .qty-up");
+    let $qty_down = $(".qty .qty-down");
+    // let $input = $(".qty .qty_input");
+
+    $qty_up.click(function(e){
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+        if($input.val() >= 1 && $input.val() <=9){
+            $input.val(function(i, oldval){
+                return ++oldval;
+            })
+        }
+    });
+
+    $qty_down.click(function(e){
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+        if($input.val() > 1 && $input.val() <= 10){
+            $input.val(function(i, oldval){
+                return --oldval;
+            })
+        }
+    });
+
+});
